f12dbac94708a933e53daa53ad227f5ea3924b2c
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-25 18:06:48 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Home } from './components/Home';
 import { Menu } from './components/Menu';
+import { DrawerNavigator } from 'react-navigation';
+
+const DrawerMenu = DrawerNavigator({
+    Home: { screen: Home }
+}, { contentComponent: Menu });
 
 export class App extends Component {
     render() {
-        return <Home />;
+        return <DrawerMenu />;
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,13 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export class Home extends Component {
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate('DrawerOpen')}>
                     <Text>Open</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate('DrawerClose')}>
                     <Text>Close</Text>
                 </TouchableOpacity>
             </View>
