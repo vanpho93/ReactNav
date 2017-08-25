230d88b56730ab4ea0b8cd886f33993408747180
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-25 15:57:15 +0700

    init for list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { SignIn } from './components/SignIn';
-import { GoPlay } from './components/GoPlay';
+import { View, Text, StyleSheet } from 'react-native';
 
 export class App extends Component {
     render() {
-        return <GoPlay />;
+        return <View style={styles.container}/>;
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#62ABDC'
+    }
+});
+
