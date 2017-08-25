e7c9c0f4454c0d425d5e617732a8ec096b00cf84
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-25 12:04:15 +0700

    src folder app component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class App extends Component {
+    render() {
+        return (
+            <Text>Hello world</Text>
+        );
+    }
+}
